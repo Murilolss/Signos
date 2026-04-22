23256a95e7b64f78e20e4316b57f9f0d8cd7b88b
lilianeperim-ifsp <liliane.perim@aluno.ifsp.edu.br>
2026-04-22 20:33:41 -0300

    Entradas e Saídas

--- main.c
+++ main.c
@@ -2,5 +2,19 @@
 
 int main(){
 
+    int dia, mes, ano;
+
+    printf("Digite seu dia de nascimento: ");
+    scanf("%d", &dia);
+
+    printf("Digite seu mes de nascimento: ");
+    scanf("%d", &mes);
+
+    printf("Digite seu ano de nascimento: ");
+    scanf("%d", &ano);
+
+    // printf("Seu signo é: ", signo);
+    // printf("Características do signo: ", caracteristicas);
+
     return 0;
 }
